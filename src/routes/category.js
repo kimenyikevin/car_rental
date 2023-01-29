727d7e4d30599ac5c28707145fb5e5434506275c
nkusikevin <nkusikvnhart@gmail.com>
2023-01-29 14:00:55 +0200

    modify update by id category route

--- src/routes/category.js
+++ src/routes/category.js
@@ -5,11 +5,16 @@ import {
   getCategory,
   getAllCategories,
   deleteCategory,
+  updateCategory,
 } from '../controllers/category';
 const router = express.Router();
 
 router.post('/', createCategory);
 router.get('/', getAllCategories);
-router.route('/:categoryid').get(getCategory).delete(deleteCategory);
+router
+  .route('/:categoryid')
+  .get(getCategory)
+  .delete(deleteCategory)
+  .put(updateCategory);
 
 export default router;
