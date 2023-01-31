99e93b80421dd3f9df9bc025a397476790f14dff
nkusikevin <nkusikvnhart@gmail.com>
2023-01-31 18:23:45 +0200

    adding update user by uuid  fnty

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -68,12 +68,13 @@ export const updateUser = async (req, res) => {
         email: req.body.email,
       },
     });
-    if (emailExist) {
-      return res.status(400).json({
-        status: 'error',
-        error: 'Email already exists',
-      });
-    }
+    // if (emailExist) {
+    //   console.log(emailExist);
+    //   return res.status(400).json({
+    //     status: 'error',
+    //     error: 'Email already exists',
+    //   });
+    // }
     const updatedUser = await user.update(req.body);
     res.status(200).json({
       status: 'success',
