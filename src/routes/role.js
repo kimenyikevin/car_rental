cc30e34cab102184a2b90cb707a6e09fad6ef168
nkusikevin <nkusikvnhart@gmail.com>
2023-02-08 11:37:02 +0200

    adding role update and delete routes

--- src/routes/role.js
+++ src/routes/role.js
@@ -1,10 +1,16 @@
 import express from 'express';
 
-import { createRole, getAllRoles, getRole } from '../controllers/roles';
+import {
+  createRole,
+  getAllRoles,
+  getRole,
+  updateRole,
+  deleteRole,
+} from '../controllers/roles';
 const router = express.Router();
 
 router.post('/', createRole);
 router.get('/', getAllRoles);
-router.route('/:roleId').get(getRole);
+router.route('/:roleId').get(getRole).delete(deleteRole).put(updateRole);
 
 export default router;
