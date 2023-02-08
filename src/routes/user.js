f357b8fe8a71b991520bb1e7b66767914e0af2e3
nkusikevin <nkusikvnhart@gmail.com>
2023-02-08 15:15:07 +0200

    adding auth admin middleware

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,13 +10,13 @@ import {
 } from '../controllers/users';
 const router = express.Router();
 
-router.post('/signup', signup);
 router.post('/signin', signIn);
+router.route('/signup').post(Auth, admin, signup);
 router.route('/').get(Auth, admin, getAllUsers);
 router
   .route('/:uuid')
-  .get(Auth, getSingleUser)
-  .put(Auth, updateUser)
-  .delete(Auth, deleteUser);
+  .get(Auth, admin, getSingleUser)
+  .put(Auth, admin, updateUser)
+  .delete(Auth, admin, deleteUser);
 
 export default router;
