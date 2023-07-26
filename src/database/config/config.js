874b10a53a668956d7c40ad80471fa0bc8bc9b5d
Kevin Kimenyi <kimenyikevin@gmail.com>
2023-07-26 23:16:59 +0200

    add build

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   production: {
     JWT_SECRET: process.env.JWT_SECRET,
-    url: process.env.PROD_DB_URL,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     port: process.env.PORT,
     sendgridAPIKey: process.env.SENGRID_API_KEY,
