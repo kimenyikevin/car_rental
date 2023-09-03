ed3a7f7c72a257b5eab4c79015592090cec32377
Kevin Kimenyi <kimenyikevin@gmail.com>
2023-09-03 17:30:30 +0200

    deploy update

--- src/controllers/foodMenuController.js
+++ src/controllers/foodMenuController.js
@@ -24,6 +24,43 @@ export const creatFoodMenu = async (req, res) => {
     }
 }
 
+export const updateFoodMenu = async (req, res) => {
+        try {
+
+            const menu = await FoodMenu.findOne({
+                where : {
+                    uuid: req.params.id
+                }
+            });
+
+            if(!menu){
+                      return res.status(404).json({
+                        status: 'error',
+                        error: 'Menu not found',
+                      });
+                
+            }
+          const { name, amount, url } = req.body;
+
+          const newFoodMenu = await menu.update({
+            name,
+            amount,
+            url,
+          });
+
+          return res.status(200).json({
+            status: 'success',
+            data: newFoodMenu,
+          });
+        } catch (error) {
+          res.status(500).json({
+            status: 'fail',
+            message: 'Error while creating a report',
+            err: error.message,
+          });
+        }
+}
+
 
 export const getAllFoodMenu = async (req, res) => {
     try {
--- src/routes/foodMenu.js
+++ src/routes/foodMenu.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { Auth, admin } from '../utils/middlewares/Authmiddleware';
 import {
     creatFoodMenu,
-    getAllFoodMenu
+    getAllFoodMenu,
+    updateFoodMenu
 } from '../controllers/foodMenuController';
 const router = express.Router();
 
 router.post('/', Auth, creatFoodMenu);
+router.put('/:id', Auth, updateFoodMenu);
 router.get('/', getAllFoodMenu);
 
 
